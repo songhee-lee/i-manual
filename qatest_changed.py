14cb63cde42c815efa3633b519a91d04627bc7ea
songhee-lee <54855238+songhee-lee@users.noreply.github.com>
2021-08-25 21:14:32 +0900

    Update qatest_changed.py

--- qatest_changed.py
+++ qatest_changed.py
@@ -5,6 +5,69 @@ import argparse
 import json
 from difflib import SequenceMatcher
 
+def remove_white_space(answer):
+
+    if not answer:
+        return answer
+
+    # 작은 따옴표의 개수에 따라 시작 위치 변경
+    toggle_c = answer.count("'")
+
+    if toggle_c and toggle_c % 2 == 0 :  #짝수개일 경우 시작부분부터 시작  카운트
+        toggle = True
+    else :
+        toggle = False      #홀수개일 경우 끝부분부터 시작 카운트
+
+    tokens = answer.split()
+    l_space = ["‘"]  # 다음 토큰에 붙어야 하는 토큰
+    r_space = [",", ".","!","?", "’",")", "~","%"] # 이전 토큰에 붙어야하는 토큰
+    numbers = ["0", "1","2","3","4","5","6","7","8","9"]
+    n_space = ["(","/"] # 앞뒤 토큰 모두 붙어야 하는 토큰
+    length = len(tokens)
+
+    result = []
+    result.append(tokens[0])
+    l_s = False
+     
+    for i in range(1, length):
+
+        # 앞 뒤로 다 붙어야 하는 경우
+        if (tokens[i-1] == "'" and toggle) or (tokens[i-1]==")" and len(tokens[i])==1):
+            result[-1] = result[-1] + tokens[i]
+            continue
+
+        if tokens[i] in n_space or (tokens[i-1]=="." and tokens[i] in numbers):
+            result[-1] = result[-1] + tokens[i]
+            l_s = True
+            continue
+
+        # 다음 토큰에 붙어야 하는 경우
+        if tokens[i] in l_space or (tokens[i] == "'" and toggle):
+            l_s = True
+
+            result.append(tokens[i])
+
+            if tokens[i] == "'":
+                toggle = False
+
+            continue
+
+        # 앞 토큰에 붙어야 하는 경우
+        if tokens[i] in r_space or l_s or (tokens[i] == "'" and toggle==False):
+            result[-1] = result[-1]+tokens[i]
+            l_s = False
+
+            if tokens[i] == "'":
+                toggle = True
+            continue
+
+        result.append(tokens[i])
+
+
+    return " ".join(result)
+
+
+
 class QuestionAnswering():
     """I-manual question answering class.
     Attributes:
@@ -48,7 +111,8 @@ class QuestionAnswering():
         answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
         
         answer = self.tokenizer.convert_tokens_to_string(self.tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-
+        answer = remove_white_space(answer)
+        
         return { "question" : question,
                  "answer" : answer,
                  "input_ids" : input_ids,
