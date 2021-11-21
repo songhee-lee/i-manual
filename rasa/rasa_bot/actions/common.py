5db727cfdcd54cfd3e8df15867f73a85bf148b50
songhee-lee <54855238+songhee-lee@users.noreply.github.com>
2021-11-21 20:39:46 +0900

    Add remove_white_space
    remove_white_space 함수 추가

--- rasa/rasa_bot/actions/common.py
+++ rasa/rasa_bot/actions/common.py
@@ -3,7 +3,6 @@ import torch
 import pandas as pd
 import random
 from pymongo import MongoClient
-import qa
 
 # MongoDB setting
 my_client = MongoClient("mongodb://localhost:27017/")
@@ -57,7 +56,7 @@ def koelectra_qa_getanswer(context, question, metadata=None, qa_step=''):
         answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
     answer = tokenizer.convert_tokens_to_string(
         tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-    answer = qa.remove_white_sapce(answer)
+    answer = remove_white_sapce(answer)
     
     return answer
 
@@ -251,3 +250,68 @@ def unego_get_question(ct, unego_count, defined=False):
         unego_comment = "인내심을 가지고 정보가 흡수되는 과정을 즐기면 자연스럽게 알게 될 테니 조급해 하지 마세요."
 
     return [question, ego_comment, unego_comment]
+
+def remove_white_space(answer):
+
+    if not answer:
+        return answer
+
+    # 작은 따옴표의 개수에 따라 시작 위치 변경
+    toggle_c = answer.count("'")
+    
+    if toggle_c and toggle_c % 2 == 0 :  #짝수개일 경우 시작부분부터 시작  카운트
+        toggle = True
+    else :
+        toggle = False      #홀수개일 경우 끝부분부터 시작 카운트
+   
+    tokens = answer.split()
+    l_space = ["‘"]  # 다음 토큰에 붙어야 하는 토큰
+    r_space = [",", ".","!","?",")", "~","%"] # 이전 토큰에 붙어야하는 토큰
+    numbers = ["0", "1","2","3","4","5","6","7","8","9"]
+    n_space = ["(","/","’"] # 앞뒤 토큰 모두 붙어야 하는 토큰
+    length = len(tokens)
+
+    result = []
+    result.append(tokens[0])
+    l_s = False
+    for i in range(1, length):
+         
+        # 앞 뒤로 다 붙어야 하는 경우
+        if tokens[i] in n_space:
+            result[-1] = result[-1] + tokens[i]
+            l_s = True
+            continue
+        
+        if (tokens[i-1] =="~" or tokens[i-1]==".") and tokens[i][0] in numbers:
+            result[-1] = result[-1] + tokens[i]
+            continue
+
+        if (tokens[i-1] == "'" and toggle) or (tokens[i-1]==")" and len(tokens[i])==1):
+            result[-1] = result[-1] + tokens[i]
+            continue
+        
+        # 다음 토큰에 붙어야 하는 경우
+        if tokens[i] in l_space or (tokens[i] == "'" and toggle):
+            l_s = True
+            
+            result.append(tokens[i])
+            
+            if tokens[i] == "'":
+                toggle = False
+
+            continue
+        
+        # 앞 토큰에 붙어야 하는 경우
+        if (tokens[i] in r_space) or l_s or (tokens[i] == "'" and toggle==False):
+            result[-1] = result[-1] + tokens[i]
+            l_s = False
+
+            if tokens[i] == "'":
+                toggle = True
+            
+            continue
+        
+        result.append(tokens[i])
+        
+
+    return " ".join(result)
