5643eaf0f32b047f73cee611f787b23c3dc6076e
Yeonsu Lee <rosielee7777@gmail.com>
2022-10-30 16:11:43 +0900

    rasa_test update

--- rasa_test/rasa_bot/actions/ninei_smalltalk_actions.py
+++ rasa_test/rasa_bot/actions/ninei_smalltalk_actions.py
@@ -15,17 +15,14 @@ smalltalk_question = []
 smalltalk_question.append(smalltalk_question_csv['korean'].values.tolist())
 smalltalk_question.append(smalltalk_question_csv['english'].values.tolist())
 smalltalk_question.append(smalltalk_question_csv['voiceID'].values.tolist())
-smalltalk_question.append(smalltalk_question_csv['cur_step'].values.tolist())
-smalltalk_question.append(smalltalk_question_csv['nest_step'].values.tolist())
-smalltalk_question.append(smalltalk_question_csv['buttons_number'].values.tolist())
 
 smalltalk_answer_csv = pd.read_csv("./data/smalltalk_answer.csv")
 smalltalk_answer = []
 smalltalk_answer.append(smalltalk_answer_csv['korean'].values.tolist())
 smalltalk_answer.append(smalltalk_answer_csv['english'].values.tolist())
 
-ninei_members = [['민준', '반', '베리', '서원', '위니', '이든', '제원', '주형', '지호', '태훈'],
-['MIN JUN', 'VAHN', 'VARI', 'SEO WON', 'WINNIE', 'EDEN', 'JE WON','JOO HYOUNG','JI HO','TAE HUN']]
+ninei_members = [['', '민준', '반', '베리', '서원', '위니', '이든', '제원', '주형', '지호', '태훈'],
+['', 'MIN JUN', 'VAHN', 'VARI', 'SEO WON', 'WINNIE', 'EDEN', 'JE WON','JOO HYOUNG','JI HO','TAE HUN']]
 
 
 class ActionSmalltalkFirst(Action):
@@ -37,7 +34,7 @@ class ActionSmalltalkFirst(Action):
 
         metadata = extract_metadata_from_tracker(tracker)
         smalltalk_step = tracker.get_slot('smalltalk_step')
-        ninei = metadata['member'] - 1 # 메타데이터 1~ 10 --> 0 ~ 9
+        ninei = metadata['member']
         lang = metadata['lang']
 
         # 첫인사 끝
@@ -45,7 +42,7 @@ class ActionSmalltalkFirst(Action):
             return [FollowupAction(name='action_start')]
 
         # buttons 요소 2개 이상
-        if smalltalk_step in [3]:
+        if smalltalk_step in [3, 22, 25]:
             buttons = []
             if smalltalk_step == 3:
                 question = smalltalk_question[lang][smalltalk_step].format(metadata['pn'], ninei_members[lang][ninei])
@@ -57,12 +54,58 @@ class ActionSmalltalkFirst(Action):
                     {"title": smalltalk_answer[lang][3], "payload": "/change_smalltalk_step{\"smalltalk_step\":5}"}
                 )
                 buttons.append(
-                    {"title": smalltalk_answer[lang][4], "payload": "/change_smalltalk_step{\"smalltalk_step\":6}"}
+                    {"title": smalltalk_answer[lang][4], "payload": "/change_smalltalk_step{\"smalltalk_step\":5}"}
                 )
                 buttons.append(
-                    {"title": smalltalk_answer[lang][5], "payload": "/change_smalltalk_step{\"smalltalk_step\":7}"}
+                    {"title": smalltalk_answer[lang][5], "payload": "/change_smalltalk_step{\"smalltalk_step\":5}"}
+                )
+            elif smalltalk_step == 22:
+                question = smalltalk_question[lang][smalltalk_step].format(metadata['pn'], ninei_members[lang][ninei])
+                vID = smalltalk_question[2][smalltalk_step]
+                dispatcher.utter_message(json_message={
+                    "type": "voiceID", "sender": metadata['uID'], "content": "{0}/{1}/{2}.wav".format(lang, ninei, vID), "data": question
+                })
+                buttons.append(
+                    {"title": smalltalk_answer[lang][22], "payload": "/change_smalltalk_step{\"smalltalk_step\":24}"}
+                )
+                buttons.append(
+                    {"title": smalltalk_answer[lang][23], "payload": "/change_smalltalk_step{\"smalltalk_step\":24}"}
+                )
+                buttons.append(
+                    {"title": smalltalk_answer[lang][24], "payload": "/change_smalltalk_step{\"smalltalk_step\":24}"}
+                )
+            elif smalltalk_step == 25:
+                question = smalltalk_question[lang][smalltalk_step].format(metadata['pn'], ninei_members[lang][ninei])
+                vID = smalltalk_question[2][smalltalk_step]
+                dispatcher.utter_message(json_message={
+                    "type": "voiceID", "sender": metadata['uID'], "content": "{0}/{1}/{2}.wav".format(lang, ninei, vID), "data": question
+                })
+                buttons.append(
+                    {"title": smalltalk_answer[lang][25], "payload": "/change_smalltalk_step{\"smalltalk_step\":28}"}
+                )
+                buttons.append(
+                    {"title": smalltalk_answer[lang][26], "payload": "/change_smalltalk_step{\"smalltalk_step\":28}"}
+                )
+                buttons.append(
+                    {"title": smalltalk_answer[lang][27], "payload": "/change_smalltalk_step{\"smalltalk_step\":28}"}
+                )
+                buttons.append(
+                    {"title": smalltalk_answer[lang][28], "payload": "/change_smalltalk_step{\"smalltalk_step\":28}"}
                 )
             dispatcher.utter_message(buttons=buttons)
+
+        # buttons 요소 없음
+        elif smalltalk_step in [31, 33]:
+            question = smalltalk_question[lang][smalltalk_step].format(metadata["pn"], ninei_members[lang][ninei])
+            vID = smalltalk_question[2][smalltalk_step]
+
+            dispatcher.utter_message(json_message={
+                "type": "voiceID", "sender": metadata['uID'], "content": "{0}/{1}/{2}.wav".format(lang, ninei, vID),
+                "data": question
+            })
+
+            return [FollowupAction(name="action_change_smalltalk_step")]
+
         # buttons 요소 1개
         else:
             question = smalltalk_question[lang][smalltalk_step].format(metadata["pn"], ninei_members[lang][ninei])
@@ -89,16 +132,14 @@ class ActionChangeSmalltalkStep(Action):
 
         smalltalk_step = tracker.get_slot('smalltalk_step')
 
-        if smalltalk_step == 10: # 나인아이 멤버 자기소개 단계일 때
+        if smalltalk_step in range(6, 9):
+            return [SlotSet("smalltalk_step", 9), FollowupAction(name="action_smalltalk_first")]
+        elif smalltalk_step == 10: # 나인아이 멤버 자기소개 단계일 때
             metadata = extract_metadata_from_tracker(tracker)
             ninei = metadata['member']
-            return [SlotSet("smalltalk_step", smalltalk_step +ninei), FollowupAction(name="action_smalltalk_first")]
-        elif smalltalk_step in range(11,21):
+            return [SlotSet("smalltalk_step", smalltalk_step + ninei), FollowupAction(name="action_smalltalk_first")]
+        elif smalltalk_step in range(11, 21):
             return [SlotSet("smalltalk_step", 21), FollowupAction(name="action_smalltalk_first")]
-        elif smalltalk_step == 22:
-            return [SlotSet("smalltalk_step", smalltalk_step +3), FollowupAction(name="action_smalltalk_first")]
-        elif smalltalk_step == 25:
-            return [SlotSet("smalltalk_step", smalltalk_step +4), FollowupAction(name="action_smalltalk_first")]    
 
 
         return [SlotSet("smalltalk_step", smalltalk_step + 1), FollowupAction(name="action_smalltalk_first")]
