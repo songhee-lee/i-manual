0af542518832956d0f42db0f76a9fb3bbc206746
JeongHyeWon <acm0209@naver.com>
2022-11-05 20:50:24 +0900

    test for sentiment analysis

--- rasa_test/rasa_bot/actions/question_actions.py
+++ rasa_test/rasa_bot/actions/question_actions.py
@@ -377,7 +377,7 @@ class ActionDefaultFallback(Action):
                 elif user_text in neutral_list:
                     user_response_type = 0
                 else:
-                    user_response_type = sentiment_predict(question, user_text, lang)
+                    user_response_type = sentiment_predict(question, user_text) ### 여기가 문제다!
                     dispatcher.utter_message("감성분석 발생 : ", user_response_type) # 디버깅을 위한 것 -> sentiment_result 값 보기
 
                 if user_response_type == 0:
